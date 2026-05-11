0e93aa2fd0f23b8a77225e5db8089c21efb236f2
Pedro Gadelha <pgpgadelha123@gmail.com>
2026-05-11 11:16:18 -0300

    feat(Security e Config): Criação das classes de Security e Configs

---
+++ src/main/java/com/gadelha/to_do_list/config/OpenApiConfig.java
@@ -0,0 +1,12 @@
+package com.gadelha.to_do_list.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Task Manager API", version = "1.0", description = "API RESTful de gerenciamento de tarefas com autenticação JWT"))
+@SecurityScheme(name = "bearer-key", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT")
+public class OpenApiConfig {}
